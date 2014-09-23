35711277704c9aa953fc0d94df17e3b4b28b27b0
Max Hinkley <maxhinkley@stmarksschool.org>
2014-09-23 14:56:25 -0400

    Comitting Everything!

---
+++ Max1/src/Max1.java
@@ -0,0 +1,26 @@
+import javax.swing.JOptionPane;
+
+
+public class Max1 {
+	
+	public static void main(String[] args){
+		
+		int x = -1;
+		boolean amISane = true;
+		String myName = "Chris";
+		double bobbert = 3.1415;
+		//float otherdec = f3.14;
+		
+		System.out.println("Hello there, Max");
+		
+		myName = JOptionPane.showInputDialog(null, "what is your name?");
+		JOptionPane.showConfirmDialog(null, ("Hi " + myName + " how are you."));
+		
+		
+		
+		
+	}
+	
+	
+
+}
---
+++ MaxGreenDot/src/MaxGreenDot.java
@@ -0,0 +1,18 @@
+
+public class MaxGreenDot {
+
+	
+	
+	
+	public static void main(String[] args) {
+		
+		
+		MaxFrame m;
+		
+		//constructor call  special
+		m= new MaxFrame(200, 400);
+		m.setVisible(true);
+		
+	}
+
+}
