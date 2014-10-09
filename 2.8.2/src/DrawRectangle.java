345a841a798857c7a37fdf58fe702bcfb4231984
maxinkley <maxhinkley@stmarksschool.org>
2014-10-09 12:09:50 -0400

    2.8.2 finished

---
+++ 2.8.2/src/DrawRectangle.java
@@ -0,0 +1,19 @@
+import objectdraw.*;
+import java.awt.*;
+
+public class DrawRectangle extends WindowController {
+	private FramedRect box;
+
+	public void onMousePress(Location pressPoint) {
+		box = new FramedRect(pressPoint, 50, 50, canvas);
+	}
+
+	public void onMouseDrag(Location pressPoint) {
+		box.moveTo(pressPoint);
+	}
+
+	public void onMouseRelease(Location pressPoint) {
+		box.removeFromCanvas();
+		new FilledRect(pressPoint, 40, 90, canvas);
+	}
+}
