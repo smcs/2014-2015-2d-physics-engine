042ee6cc5ba30c24319f2b3c3d0bf298280e8ac2
maxinkley <maxhinkley@stmarksschool.org>
2014-10-27 12:22:00 -0400

    finished random box w/out colors

--- 3.12.3/src/RandomBox.java
+++ 3.12.3/src/RandomBox.java
@@ -2,42 +2,35 @@ import objectdraw.*;
 import static java.lang.System.out;
 
 public class RandomBox extends WindowController {
-	private int boxExists = 1;
-	private double height = Math.random() * 100 + 20;
-	private double width = Math.random() * 100 + 20;
-	private Location startPoint = new Location(100-height/2,100-width/2);
-	
+	private double height;
+	private double width;
+	private Location startPoint;
+
 	private FilledRect box;
-	
+
 	public void begin() {
+		drawBox();
+	}
+
+	public void drawBox() {
+		canvas.clear();
+		height = Math.random() * 100 + 20;
+		width = Math.random() * 100 + 20;
+		startPoint = new Location(100 - height / 2, 100 - width / 2);
 		box = new FilledRect(startPoint, height, width, canvas);
 	}
-	
-	
-	public void onMousePress(Location pressPoint){
-		System.out.println("Location:" + startPoint );
-		System.out.println("Height:" + height );
-		System.out.println("Width:" + width );
-	}
-	
-	
-	public void onMouseMove(){
-		if(boxExists == 0){
-			box = new FilledRect(startPoint, height, width, canvas);
-		}
-		}
-	
-	public void onMouseExit(){
-		System.out.println("onMouseExit works");
-		//box.removeFromCanvas();//
-		boxExists = 0;
-	}
-		
-		
-		
-		
-		
-		
+
+	public void onMousePress(Location pressPoint) {
+		System.out.println("Location:" + startPoint);
+		System.out.println("Height:" + height);
+		System.out.println("Width:" + width);
 	}
 
+	public void onMouseEnter(Location pressPoint) {
+		drawBox();
+	}
 
+	public void onMouseExit(Location pressPoint) {
+		canvas.clear();
+	}
+}
