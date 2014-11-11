a5dbed0cedd785bf10f10a5e3df4a1ec8ee88492
maxinkley <maxhinkley@stmarksschool.org>
2014-11-11 14:52:37 -0500

    tank

--- Tank/src/Tank.java
+++ Tank/src/Tank.java
@@ -1,4 +1,16 @@
 
-public class Tank {
+public class Tank extends OTOS{
 
+	OTOS tank;
+	double health;
+	double heat;
+	
+	tank = new OTOS();
+	
+	Tank();
+		tank.init( getImage(RedTankRight.gif) 30, 30, 60, 40)
+	}
+	
+	
+	
 }
