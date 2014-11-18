cfd023675763f833692bab1d7aab1bbeb9dd8b86
Seth Battis <sethbattis@stmarksschool.org>
2014-11-18 14:15:06 -0500

    Threaded Bullet

--- Tank/src/Bullet.java
+++ Tank/src/Bullet.java
@@ -1,15 +1,18 @@
-import java.awt.Image;
+import java.awt.*;
 
-import objectdraw.DrawingCanvas;
+import objectdraw.*;
 
 
-public class Bullet extends OTOS{
+public class Bullet extends OTOS implements Runnable {
 
 	Bullet( Image image, int x, int y, int heading, DrawingCanvas canvas){
 		super (image, x, y, 5, 5 canvas);
+		
+		new Thread(this).start();
+	}
+	
+	public void run() {
 		while//not touching another otos
 			move in heading
 	}
-	
-	
 }
