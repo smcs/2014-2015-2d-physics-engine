cd79139827d82450dda57c13d1a07dca3b33f4ae
Seth Battis <sethbattis@stmarksschool.org>
2014-12-09 14:37:04 -0500

    Don't collide with yourself

--- Tank/src/Bullet.java
+++ Tank/src/Bullet.java
@@ -1,53 +1,51 @@
 import java.awt.*;
 import java.util.*;
 
-
 import objectdraw.*;
-
 
 public class Bullet extends OTOS implements Runnable {
 
 	private char heading;
-	
-	
-	Bullet(Image i, int x, int y, char h, DrawingCanvas canvas, Vector<OTOS> everybody){
-		super (i, x, y, 5, 5, canvas);
-		this.everybody = everybody;
-		heading = h;
-		new Thread(this).start();
-		everybody.add(this);
-	}
-	Bullet(Image i, double x, double y, char h, DrawingCanvas canvas, Vector<OTOS> everybody){
-		super (i, x, y, 5, 5, canvas);
-		this.everybody = everybody;
-		heading = h;
-		new Thread(this).start();
-		everybody.add(this);
-	}
-	
-	
 
-	
-	
+	Bullet(Image i, int x, int y, char h, DrawingCanvas canvas,
+			Vector<OTOS> everybody) {
+		super(i, x, y, 5, 5, canvas);
+		this.everybody = everybody;
+		heading = h;
+		new Thread(this).start();
+		everybody.add(this);
+	}
+
+	Bullet(Image i, double x, double y, char h, DrawingCanvas canvas,
+			Vector<OTOS> everybody) {
+		super(i, x, y, 5, 5, canvas);
+		this.everybody = everybody;
+		heading = h;
+		new Thread(this).start();
+		everybody.add(this);
+	}
+
 	OTOS hitObject;
-	
+
 	public void run() {
-		while(overlaps().size() != 0){
-			if(heading == 'w'){
-				this.moveTo(this.getX(), this.getY()-2);
+		Vector<OTOS> overlaps;
+		do {
+			if (heading == 'w') {
+				this.moveTo(this.getX(), this.getY() - 2);
 			}
-			if(heading == 'a'){
-				this.moveTo(this.getX()-2, this.getY());
+			if (heading == 'a') {
+				this.moveTo(this.getX() - 2, this.getY());
 			}
-			if(heading == 's'){
-				this.moveTo(this.getX(), this.getY()+2);
+			if (heading == 's') {
+				this.moveTo(this.getX(), this.getY() + 2);
 			}
-			if(heading == 'a'){
-				this.moveTo(this.getX()+2, this.getY());
+			if (heading == 'a') {
+				this.moveTo(this.getX() + 2, this.getY());
 			}
-		}
-		
-		hitObject = overlaps().elementAt(0);
-		
+			overlaps = overlaps();
+		} while (overlaps.size() == 0);
+
+		hitObject = overlaps.elementAt(0);
+
 	}
 }
--- Tank/src/OTOS.java
+++ Tank/src/OTOS.java
@@ -2,70 +2,81 @@ import java.awt.*;
 import java.util.*;
 
 import objectdraw.*;
-	
-
 
 public class OTOS {
 
 	protected VisibleImage sprite;
-	
-	protected Vector <OTOS> everybody;
-	
-	
-	public Vector<OTOS> getEverybody(){
+
+	protected Vector<OTOS> everybody;
+
+	public Vector<OTOS> getEverybody() {
 		return everybody;
 	}
-	
-	public DrawingCanvas getCanvas(){
+
+	public DrawingCanvas getCanvas() {
 		return sprite.getCanvas();
 	}
-	
-	public OTOS(Image image, double x, double y, double width, double height, DrawingCanvas canvas){
+
+	public OTOS(Image image, double x, double y, double width, double height,
+			DrawingCanvas canvas) {
 		sprite = new VisibleImage(image, x, y, width, height, canvas);
 	}
 
-	void moveTo(double x, double y){
-		sprite.moveTo(x,y);
+	void moveTo(double x, double y) {
+		sprite.moveTo(x, y);
 	}
-	
-	void moveTo(Location l){
+
+	void moveTo(Location l) {
 		sprite.moveTo(l);
 	}
-	
-	void setImage(java.awt.Image image){
+
+	void setImage(java.awt.Image image) {
 		sprite.setImage(image);
 	}
-	
-	boolean contains(Location point){
+
+	boolean contains(Location point) {
 		return sprite.contains(point);
 	}
-	
-	double getX(){
+
+	double getX() {
 		return sprite.getX();
 	}
-	
-	double getY(){
+
+	double getY() {
 		return sprite.getY();
 	}
-	
-	Vector<OTOS> overlaps(){
+
+	synchronized Vector<OTOS> overlaps() {
 		Vector<OTOS> overlapped = new Vector<OTOS>();
-		
-		for (int counter = 0; counter<everybody.size(); counter++) {
+
+		for (int counter = 0; counter < everybody.size(); counter++) {
 			VisibleImage other = everybody.get(counter).sprite;
-			
-			if(   (other.getX() > sprite.getX() && other.getX() < sprite.getX()+sprite.getWidth())   ||    (other.getX() + other.getWidth() > sprite.getX() && other.getX() + other.getWidth() < sprite.getX()+sprite.getWidth())               &&        (other.getY() > sprite.getY() && other.getY() < sprite.getY()+sprite.getHeight())    ||       (other.getY() + other.getHeight() > sprite.getY() && other.getY() + other.getHeight() < sprite.getY()+sprite.getHeight()) ){
-				overlapped.add(everybody.get(counter));
+			if (other != this.sprite) {
+
+				if ((other.getX() > sprite.getX() && other.getX() < sprite
+						.getX() + sprite.getWidth())
+						|| (other.getX() + other.getWidth() > sprite.getX() && other
+								.getX() + other.getWidth() < sprite.getX()
+								+ sprite.getWidth())
+						&& (other.getY() > sprite.getY() && other.getY() < sprite
+								.getY() + sprite.getHeight())
+						|| (other.getY() + other.getHeight() > sprite.getY() && other
+								.getY() + other.getHeight() < sprite.getY()
+								+ sprite.getHeight())) {
+					overlapped.add(everybody.get(counter));
+				}
+				// left xvalue of other is within sprite left and right x values
+				// right xvalue of other is within sprite left and right x
+				// values upper y value of other is contained by sprite y values
+				// lower other y value is contained by sprite y values
+				// { X value of other is contained by x values of sprite } { X
+				// value of other is contained by x values of sprite }
+
 			}
-//      				left xvalue of other is within sprite left and right x values														right xvalue of other is within sprite left and right x values															upper y value of other is contained by sprite y values											lower other y value is contained by sprite y values
-//    				{                               								X value of other is contained by x values of sprite																							}						{														X value of other is contained by x values of sprite																													}
-		
-			
-			
+
 		}
 		return overlapped;
-		
+
 	}
-	
 
 }
