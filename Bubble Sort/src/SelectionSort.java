16a98483acc178c79aafe04a12c1bece7b98a2f1
Seth Battis <sethbattis@stmarksschool.org>
2015-01-15 09:18:10 -0500

    Reset Flag

--- Bubble Sort/src/SelectionSort.java
+++ Bubble Sort/src/SelectionSort.java
@@ -1,32 +1,26 @@
 import java.util.Vector;
 
-
 public class SelectionSort {
 
-	private Vector <Integer> sorted;
-	private int flag = 0;
-	
-	void sort(Vector <Integer> unsorted){
-		sorted = new Vector <Integer> ();
-		while (unsorted.size() > 0){
-			
-			for(int i = 0; i < unsorted.size(); i++){
-				if(unsorted.get(i)>unsorted.get(flag)){
+	void sort(Vector<Integer> unsorted) {
+		Vector<Integer> sorted = new Vector<Integer>();
+		while (unsorted.size() > 0) {
+			int flag = 0;
+
+			for (int i = 0; i < unsorted.size(); i++) {
+				if (unsorted.get(i) > unsorted.get(flag)) {
 					flag = i;
 				}
 			}
 			sorted.add(0, unsorted.get(flag));
 			unsorted.remove(flag);
-			
-			
+
 		}
-		
-		
-		
-		for(int i = 0; i<sorted.size(); i++){
+
+		for (int i = 0; i < sorted.size(); i++) {
 			System.out.print(sorted.get(i) + " ");
 		}
 		System.out.println();
 	}
-	
+
 }
