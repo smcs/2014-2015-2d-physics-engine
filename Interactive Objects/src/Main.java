88cd9b78de72a91e8ab9d35eac1264b2909adfb9
maxinkley <maxhinkley@stmarksschool.org>
2015-04-06 10:13:26 -0400

    able to tell which object i am hitting

--- Interactive Objects/src/Main.java
+++ Interactive Objects/src/Main.java
@@ -12,15 +12,19 @@ public class Main extends WindowController {
 	
 	
 	private InteractiveObject redSprite;
+	private InteractiveObject greenSprite;
 	private InteractiveObject blueSprite;
 	
 	
 public void begin(){
-	Image red, blue;
+	Image red, blue, green;
 	red = getImage("Images/Red.png");
 	blue = getImage("Images/Blue.png");
-	this.setSize(1365,800);
+	green = getImage("Images/Green.png");
+	//this.setSize(1365,800); fullscreen
+	this.setSize(600,500); 
 	redSprite = new InteractiveObject(red, 100, 100, 50, 50, canvas);
+	greenSprite = new InteractiveObject(green, 400, 400, 50, 50, canvas);
 	blueSprite = new InteractiveObject(blue, 200, 200, 50, 50, canvas);
 	
 	}
@@ -28,8 +32,17 @@ public void begin(){
 	
 	public void onMousePress(Location l){
 		blueSprite.moveTo(l);
+		
 		if(blueSprite.overlaps().size()>0){
-			System.out.println("hit");
+			System.out.print("hit: ");
+		}
+		
+		if(blueSprite.overlaps().contains(redSprite)){
+			System.out.println("red");
+		}
+		
+		if(blueSprite.overlaps().contains(greenSprite)){
+			System.out.println("green");
 		}
 		
 	}
